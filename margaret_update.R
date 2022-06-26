302a488e250ea96e19602e4a200cde3c51481191
Camilo García <72902253+camilogs1@users.noreply.github.com>
2022-06-26 00:20:44 -0500

    Update

--- margaret_update.R
+++ margaret_update.R
@@ -1,8 +1,8 @@
 library(margaret)
 library(tidyverse)
 
-groups <- read_csv("https://docs.google.com/spreadsheets/d/1gBaXHFp1NTUTeXodb4JyHqY-P-AWV5yN5-p4L1O09gk/export?format=csv&gid=0")
-researchers  <-  read_csv("https://docs.google.com/spreadsheets/d/1gBaXHFp1NTUTeXodb4JyHqY-P-AWV5yN5-p4L1O09gk/export?format=csv&gid=347194763")
+groups <- read_csv("https://docs.google.com/spreadsheets/d/1OFg9Jzypg_uwsmFxyXFSgtdfXfgsN9AG/export?format=csv&gid=808772593")
+researchers  <-  read_csv("https://docs.google.com/spreadsheets/d/1mpwR15wHbrFbO1s8DWsDdLta_aou7gHD/export?format=csv&gid=2102359855")
 
 margaret_updated <-
   getting_data(groups, researchers)
