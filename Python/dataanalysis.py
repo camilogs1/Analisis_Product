8a8511a277b99a21eacd591bcc0fbe1e1a6072c5
Camilo García <72902253+camilogs1@users.noreply.github.com>
2022-07-02 23:00:37 -0500

    Update dataanalysis.py

--- Python/dataanalysis.py
+++ Python/dataanalysis.py
@@ -1,8 +1,19 @@
 import numpy as np
 import pandas as pd
 
+## Lectura de archivos
 papers = pd.read_csv('https://docs.google.com/spreadsheets/d/1OFg9Jzypg_uwsmFxyXFSgtdfXfgsN9AG/export?format=csv&gid=808772593')
-papers1 = papers['ano'] > 2014
+papers['ano'] = pd.to_numeric(papers['ano'], errors='coerce')
+papers = papers.dropna(subset=['ano'])
+papers['ano'] = papers['ano'].astype(int)
+papers = papers[papers['ano'] > 2014]
 
 researchers = pd.read_csv('https://docs.google.com/spreadsheets/d/1mpwR15wHbrFbO1s8DWsDdLta_aou7gHD/export?format=csv&gid=2102359855')
+researchers['universidad'] = researchers['grupo']
 
+## Investigadores en internos y externos
+authors_others = papers[['id', 'autores', 'ano']]
+
+aux = authors_others
+aux = aux.explode('autores')
+aux = aux.groupby(['id']).mean()
